d6111c3cb3a1868ac4454b53a325d968dbf8a551
Angelo Rafael Recio <recioangelorafael@gmail.com>
2023-04-04 22:39:44 +0800

    testing out routes and redirects

--- SRS/routes/web.php
+++ SRS/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return view('landingPage');
+});
+
+Route::redirect('/','/home',302);
+
+Route::get('/user',function(Request $request){
+    dd($request);
+    return 'lol';
+});
+
+Route::get('/res/{id}',function($id){
+    return response(view('landingPage',['id'=>$id]),201)
+        ->header('Content-Type','text/html');
 });
