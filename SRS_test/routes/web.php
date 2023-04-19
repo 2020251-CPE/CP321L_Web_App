467699600779b9ac1d9c0289f2873ece86987ad6
Angelo Rafael Recio <recioangelorafael@gmail.com>
2023-04-19 08:06:34 +0800

    succesfully tested adding Bootstrap CSS to project

--- SRS_test/routes/web.php
+++ SRS_test/routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Http\Request;
 */
 
 Route::get('/main',function(){ //Default Main route for Laravel
-    return view('welcome');
+    return view('app');
 });
 
 Route::get('/',[StudentController::class,'index']);
@@ -55,3 +55,6 @@ Route::get('/login',[UserController::class,'index'])->name('login');
 Route::get('/user/{id}',[UserController::class,'show']);
 
 Route::get('/students',[StudentController::class,'index']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
