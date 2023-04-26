69a0230888859bc6e95cdc1ea1e299b594e7b71d
Angelo Rafael Recio <recioangelorafael@gmail.com>
2023-04-26 21:40:13 +0800

    Moved home.blade.php to pages directory

---
+++ SelfReflectionStation/resources/views/pages/home.blade.php
@@ -0,0 +1,11 @@
+
+<div class="main row" id="landingPG">
+            <div class="col-lg-12">
+                <h1 class="display-4">Self Reflection Station</h1>
+            </div>
+            <div class="col-lg-4">PIC HERE</div>
+            <div class="col-lg-8">
+                <div><p>INTRODUCTION</p></div>
+                <div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris volutpat lorem sed ex cursus posuere. Vivamus odio neque, vestibulum ullamcorper quam non, placerat volutpat sem. Morbi hendrerit luctus justo. Nam consequat mi ac enim convallis varius. Quisque commodo neque massa, nec varius leo pharetra hendrerit. Integer placerat massa et vehicula commodo. Ut sagittis nulla non nulla hendrerit pretium. In id dolor auctor, pulvinar ante ac, venenatis neque.</p></div>
+            </div>
+        </div>
