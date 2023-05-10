5f48f58b4c15ee1f259f75b5af818352d95e1e4d
Angelo Rafael Recio <recioangelorafael@gmail.com>
2023-05-10 20:54:24 +0800

    InProgress: Added Node Modules for FrontEnd

--- SelfReflectionStation/resources/js/app.js
+++ SelfReflectionStation/resources/js/app.js
@@ -1,3 +1,6 @@
 import './bootstrap';
-
 import '../sass/app.scss'
+
+import { Autoplay, Navigation, Pagination } from "swiper";
+import Swiper from "swiper";
+Swiper.use([Autoplay, Navigation, Pagination]);
