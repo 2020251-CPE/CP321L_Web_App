9c9bd4961408bd03c6fc7b0dd609a41230af89cc
Angelo Rafael Recio <recioangelorafael@gmail.com>
2023-05-13 22:55:08 +0800

    Improved Data on AnswersTable

--- SelfReflectionStation/misc/SQL/TableCreation/SideTables.sql
+++ SelfReflectionStation/misc/SQL/TableCreation/SideTables.sql
@@ -8,8 +8,8 @@ VALUES
 
 INSERT INTO AnswersTable(AnswerType,AnswerValues)
 VALUES
-	('5 Part Probability Range', '{"0":"Never","1":"Not Likely","2":"Neutral","3":"Likely","4":"Most Likely"}'),
+	('5 Part Frequency Range', '{"0":"Never","1":"Not Frequent","2":"Sometimes","3":"Frequent","4":"Very Frequent"}'),
     ('5 Part Agreement Range', '{"0":"Strongly Disagree","1":"Disagree","2":"Neutral","3":"Agree","4":"Strongly Agree"}'),
-    ('3 Part Probability Range', '{"0":"Not Likely","1":"Neutral","2":"Most Likely"}'),
-    ('3 Part Agreement Range', '{"0":"Agree","1":"Neutral","2":"Agree"}'),
+    ('5 Part Probability Range', '{"0":"Never","1":"Unlikely","2":"50/50","3":"Likely","4":"Most Likely"}'),
+    ('3 Part Agreement Range', '{"0":"Disagree","1":"Neutral","2":"Agree"}'),
 	('Yes and No', '{"0":"No","1":"Yes"}');
