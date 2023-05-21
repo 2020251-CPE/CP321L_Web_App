e1c4a9e085c30c95bf53867b61ed2508c12a1fe8
Angelo Rafael Recio <recioangelorafael@gmail.com>
2023-05-21 22:25:58 +0800

    removed comments & non-nullability of test results

--- SelfReflectionStation/misc/SQL/TableCreation/AccountTable.sql
+++ SelfReflectionStation/misc/SQL/TableCreation/AccountTable.sql
@@ -1,49 +1,49 @@
 --Create Tables for Accounts
 CREATE TABLE Accounts (
-    id UNSIGNED NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
-    Name VARCHAR(255) NOT NULL,
+    Name varchar(255) NOT NULL,
     Age INT NOT NULL,
-    Email VARCHAR(100) NOT NULL, --should be encrypted by PHP
-    Password VARCHAR(100) NOT NULL, --should be encrypted by PHP
-    GeneralAnxietyTestAVR INT NOT NULL,
-    AlcoholAddictionTestAVR INT NOT NULL,
-    DrugAddictionTestAVR INT NOT NULL,
-    ShoppingAddictionTestAVR INT NOT NULL,
-    GamblingAddictionTestAVR INT NOT NULL
+    Email varchar(100) NOT NULL, 
+    Psswrd varchar(100) NOT NULL,
+    GeneralAnxietyTestAVR INT,
+    AlcoholAddictionTestAVR INT,
+    DrugAddictionTestAVR INT,
+    ShoppingAddictionTestAVR INT,
+    GamblingAddictionTestAVR INT
 );
 CREATE TABLE AnxietyTestResults (
-    id UNSIGNED NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
     RecordedResult INT NOT NULL,
-    Email VARCHAR(100) NOT NULL, --should be encrypted by PHP
+    Email varchar(100) NOT NULL, 
     DateAndTimeOfRecord DATETIME NOT NULL
 );
 CREATE TABLE AlcoholAddictionTestResults (
-    id UNSIGNED NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
     RecordedResult INT NOT NULL,
-    Email VARCHAR(100) NOT NULL, --should be encrypted by PHP
+    Email varchar(100) NOT NULL, 
     DateAndTimeOfRecord DATETIME NOT NULL
 );
 CREATE TABLE DrugAddictionTestResults (
-    id UNSIGNED NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
     RecordedResult INT NOT NULL,
-    Email VARCHAR(100) NOT NULL, --should be encrypted by PHP
+    Email varchar(100) NOT NULL, 
     DateAndTimeOfRecord DATETIME NOT NULL
 );
 CREATE TABLE ShoppingAddictionTestResults (
-    id UNSIGNED NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
     RecordedResult INT NOT NULL,
-    Email VARCHAR(100) NOT NULL, --should be encrypted by PHP
+    Email varchar(100) NOT NULL,
     DateAndTimeOfRecord DATETIME NOT NULL
 );
 CREATE TABLE Gambling_Addiction_Test_Results (
-    id UNSIGNED NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
     RecordedResult INT NOT NULL,
-    Email VARCHAR(100) NOT NULL, --should be encrypted by PHP
+    Email varchar(100) NOT NULL,
     DateAndTimeOfRecord DATETIME NOT NULL
 );
